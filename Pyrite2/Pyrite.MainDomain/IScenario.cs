3c1e07947e575b3caba47db34950c04aa222b451
noant <anton.novgorodcev@gmail.com>
2017-01-31 17:27:29 +0300

    MainDomain started

---
+++ Pyrite2/Pyrite.MainDomain/IScenario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyrite.MainDomain
+{
+    public interface IScenario
+    {
+        string Id { get; } //guid
+        void Execute(string param);
+        ValueType ValueType { get; }
+        void OnStateChanged(Action<IScenario> action);
+        void OnStateChanged(Action<IScenario> action, bool onlyOnce);
+    }
+}
