46108ef35339ab5dcd4c9e6ce77271254413f695
noant <noant1@yandex.ru>
2017-04-05 23:25:44 +0300

    ---

--- Pyrite2Plugins/ZWavePlugin/ZWavePlugin/ZWaveNodeValue.cs
+++ Pyrite2Plugins/ZWavePlugin/ZWavePlugin/ZWaveNodeValue.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using Pyrite.ActionsDomain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pyrite.ActionsDomain.ValueTypes;
 
 namespace ZWavePlugin
 {
-    class ZWaveNodeValue
+    public class ZWaveNodeValue : IAction
     {
+        public string Caption
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ValueChangedDelegate ValueChanged
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ValueTypeBase ValueType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string GetValue(ExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(ExecutionContext context, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UserInitializeWith(ValueTypeBase valueType)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
