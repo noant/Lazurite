74a1e4906d4aa6031a8eef1dc2b3afbe141df451
noant <noant1@yandex.ru>
2017-11-09 19:08:18 +0300

    loosed files; rebuild;

---
+++ Lazurite/LazuriteMobile.App/LazuriteMobile.App.Android/LazuriteService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+
+namespace LazuriteMobile.App.Droid
+{
+    [Service(Exported = false)]
+    public class LazuriteService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            return base.OnStartCommand(intent, flags, startId);
+        }
+    }
+
+    public class Binder : IBinder
+    {
+        public IntPtr Handle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string InterfaceDescriptor
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsBinderAlive
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dump(FileDescriptor fd, string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DumpAsync(FileDescriptor fd, string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LinkToDeath(IBinderDeathRecipient recipient, int flags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PingBinder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IInterface QueryLocalInterface(string descriptor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Transact(int code, Parcel data, Parcel reply, [GeneratedEnum] TransactionFlags flags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnlinkToDeath(IBinderDeathRecipient recipient, int flags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
