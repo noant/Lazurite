76cbe6007be6a75fd3adb991f026e28c55725dac
Admin <Admin@DESKTOP>
2017-12-25 20:32:45 +0300

    DeviceDataHandler update;

--- Lazurite/LazuriteMobile.App/LazuriteMobile.App/DeviceDataHandler.cs
+++ Lazurite/LazuriteMobile.App/LazuriteMobile.App/DeviceDataHandler.cs
@@ -1,14 +1,14 @@
-﻿using Lazurite.MainDomain;
+﻿using Lazurite.IOC;
+using Lazurite.Logging;
+using Lazurite.MainDomain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LazuriteMobile.App
 {
     public class DeviceDataHandler : IAddictionalDataHandler
     {
+        private static readonly ILogger Log = Singleton.Resolve<ILogger>();
+
         public void Handle(AddictionalData data)
         {
             //do nothing
@@ -21,15 +21,26 @@ namespace LazuriteMobile.App
 
         public void Prepare(AddictionalData data)
         {
-            var deviceInfo = new DeviceInfo();
-            deviceInfo.Name = string.Format(
-                "[Model: {0}]; [OS: {1},{2}]; [Manufacturer: {3}]; [DeviceId: {4}]",
-                Plugin.DeviceInfo.CrossDevice.Hardware.Model,
-                Plugin.DeviceInfo.CrossDevice.Hardware.OperatingSystem,
-                Plugin.DeviceInfo.CrossDevice.Hardware.OperatingSystemVersion,
-                Plugin.DeviceInfo.CrossDevice.Hardware.Manufacturer,
-                Plugin.DeviceInfo.CrossDevice.Hardware.DeviceId);
-            data.Set(deviceInfo);
+            try
+            {
+                var deviceInfo = new DeviceInfo();
+                deviceInfo.Name = string.Format(
+                    "[Model: {0}]; [OS: {1},{2}]; [Manufacturer: {3}]; [DeviceId: {4}]",
+                    Plugin.DeviceInfo.CrossDevice.Hardware.Model,
+                    Plugin.DeviceInfo.CrossDevice.Hardware.OperatingSystem,
+                    Plugin.DeviceInfo.CrossDevice.Hardware.OperatingSystemVersion,
+                    Plugin.DeviceInfo.CrossDevice.Hardware.Manufacturer,
+                    Plugin.DeviceInfo.CrossDevice.Hardware.DeviceId);
+                data.Set(deviceInfo);
+            }
+            catch (Exception e)
+            {
+                data.Set(new DeviceInfo()
+                {
+                    Name = string.Format("[UnknownDevice][error: {0}]", e.Message)
+                });
+                Log.Error("Error while getting device info", e);
+            }
         }
     }
 }
