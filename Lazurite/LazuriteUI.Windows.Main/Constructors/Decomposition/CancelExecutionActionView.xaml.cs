37064a5fc268145e02ea22b19ee5811825795bd2
Admin <Admin@DESKTOP>
2018-02-06 23:46:38 +0300

    lost files

---
+++ Lazurite/LazuriteUI.Windows.Main/Constructors/Decomposition/CancelExecutionActionView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Lazurite.ActionsDomain;
+using Lazurite.CoreActions;
+using Lazurite.CoreActions.ContextInitialization;
+using System;
+using System.Windows.Controls;
+
+namespace LazuriteUI.Windows.Main.Constructors.Decomposition
+{
+    /// <summary>
+    /// Логика взаимодействия для ExecuteActionView.xaml
+    /// </summary>
+    public partial class CancelExecutionActionView : StackPanel, IConstructorElement
+    {
+        private CancelExecutionAction _action;
+
+        public CancelExecutionActionView()
+        {
+            InitializeComponent();
+            buttons.RemoveClick += () => NeedRemove?.Invoke(this);
+            buttons.AddNewClick += () => NeedAddNext?.Invoke(this);
+            action1View.MakeButtonsInvisible();
+        }
+        
+        public ActionHolder ActionHolder
+        {
+            get;
+            private set;
+        }
+
+        public bool EditMode
+        {
+            get;
+            set;
+        }
+        
+        public IAlgorithmContext AlgorithmContext
+        {
+            get;
+            private set;
+        }
+
+        public void Refresh(ActionHolder actionHolder, IAlgorithmContext algoContext)
+        {
+            ActionHolder = actionHolder;
+            AlgorithmContext = algoContext;
+            _action = (CancelExecutionAction)actionHolder.Action;
+            action1View.Refresh(actionHolder, algoContext);
+        }
+
+        public event Action<IConstructorElement> Modified;
+        public event Action<IConstructorElement> NeedAddNext;
+        public event Action<IConstructorElement> NeedRemove;
+    }
+}
